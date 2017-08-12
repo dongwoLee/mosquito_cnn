df210a1780ca3c46b252b2a036ec425ce4ca38df
dongwoLee <dongwoo4553@gmail.com>
2017-08-12 16:30:37 +0900

    change foramt date

--- Location/process_data.py
+++ Location/process_data.py
@@ -1,10 +1,17 @@
 import csv
 import calendar
 import datetime
+import pandas as pd
 
 date_1=datetime.date(2011,5,2)
 
+dateList = []
 
 for i in range(1,31):
     end_date = date_1+datetime.timedelta(days=i)
-    print (end_date)
+    print(end_date)
+
+
+#data = pd.read_csv('Salesio_change_1.csv')
+
+
