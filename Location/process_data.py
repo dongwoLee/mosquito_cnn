85187d89fe0851e58760d4da90ed266d9bbcc383
dongwoLee <dongwoo4553@gmail.com>
2017-08-14 10:42:05 +0900

    salesio_1

--- Location/process_data.py
+++ Location/process_data.py
@@ -3,15 +3,19 @@ import calendar
 import datetime
 import pandas as pd
 
-date_1=datetime.date(2011,5,2)
+date_1=datetime.date(2011,6,1)
 
 dateList = []
 
-for i in range(1,31):
-    end_date = date_1+datetime.timedelta(days=i)
+for i in range(0,31):
+    end_date = date_1+datetime.timedelta(days=-i)
+    (dateList.append(end_date))
     print(end_date)
 
 
-#data = pd.read_csv('Salesio_change_1.csv')
+
+data = pd.read_csv('Salesio_change_1.csv')
+
+
 
 
