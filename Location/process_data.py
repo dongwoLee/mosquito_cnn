68b095b631a0b3712d36899489619cd03eda2189
dongwoLee <dongwoo4553@gmail.com>
2017-08-15 20:16:55 +0900

    working

--- Location/process_data.py
+++ Location/process_data.py
@@ -12,13 +12,26 @@ for i in range(0,31):
     end_date = standardDate+datetime.timedelta(days=-i)
     (dateList.append(str(end_date)))
 
-matrix = []
+data = pd.read_csv('Salesio_change_1.csv')
+mos = (data[str(standardDate)].tolist())
+mos = mos[-1] ## standard mosquito data
+
+wholeDate = []
 with open('Salesio_change_1.csv','r') as f:
     reader = csv.reader(f,delimiter=',')
     for row in reader:
-        matrix.append(row)
+        wholeDate.append(row)
 
-data = pd.read_csv('Salesio_change_1.csv')
+for i in range(len(dateList)):
+    for j in range(len(wholeDate[0])):
+        if(dateList[i]==wholeDate[0][j]):
+            result=data[str(dateList[i])].tolist()
+            result[-1]=mos
+            with open("/Users/leedongwoo/Desktop/mosquito_cnn/Location_allDate/Salesio_06_01.csv","a") as f:
+                wr = csv.writer(f)
+                wr.writerow(result)
+
+
 
 
 
