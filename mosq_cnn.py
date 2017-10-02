37886099bcea8d4894693d87adde892ced44d92c
dongwoLee <dongwoo4553@gmail.com>
2017-10-03 00:29:28 +0900

    make training csv

--- mosq_cnn.py
+++ mosq_cnn.py
@@ -25,24 +25,37 @@ def readCsv(csvlist):# label data should be made by this function.
 
 def readCsvTwoDimension(csvList):#training input should be made by two dimensional list.
 	
-	twoDimensionData = [] 
+	Matrix = []
 
-	return 
+	for i in range(len(csvList)):
+		with open(csvList[i],'r') as f:
+			reader = csv.reader(f,delimiter=',')
+			Matrix.append(list(reader))
+
+	return Matrix
 
 def makeTrainingSet():#training and test mosq
 	
 	location = []
-	trainCsv=[]
+	trainFolder = []
+	trainCsv = []
 	for folder in os.listdir("/Users/leedongwoo/Desktop/mosquito_cnn/Location_allDate"):
 		if folder == '.DS_Store':
 			continue
 		else:
-			location.append(folder)
+			location.append(folder)#all location
 
 	for i in range(len(location)):
-		trainCsv.extend(getFileName("/Users/leedongwoo/Desktop/mosquito_cnn/Location_allDate/"+str(location[i])))
+		trainFolder.append(getFileName("/Users/leedongwoo/Desktop/mosquito_cnn/Location_allDate/"+str(location[i])))
 
+	for j in range(len(trainFolder)):
+		for k in range(len(trainFolder[j])):
+			trainFolder[j][k] = "/Users/leedongwoo/Desktop/mosquito_cnn/Location_allDate/"+str(location[j])+"/"+str(trainFolder[j][k])
+
+	return len(trainFolder)
+	# trainFolder = getFileName("/Users/leedongwoo/Desktop/mosquito_cnn/Location_allDate/Congress/")
 	
+	# train_data = readCsvTwoDimension(trainCsv)
 	
 
 def makeLabel_level():#training level and test level 
@@ -50,18 +63,21 @@ def makeLabel_level():#training level and test level
 	train = []
 	test = []
 	wholeLabelCsv = getFileName("/Users/leedongwoo/Desktop/mosquito_cnn/Label_Data/Level/noDateMosq")
-	
 	wholeLabel = readCsv(wholeLabelCsv)
 
 	train = wholeLabel[:int(len(wholeLabel)*0.8)]
 	test = wholeLabel[int(len(wholeLabel)*0.8):len(wholeLabel)]
+
+	train = list(map(float,train))
+	test = list(map(float,test))
 
 	return array(train),array(test)
 
 
 if __name__ == '__main__':
 	
-	trainLabel,testLabel = makeLabel_level()
-	# print ((trainLabel),(testLabel))
-	print(len(makeTrainingSet()))
+	#trainLabel,testLabel = makeLabel_level()
+	# print(readCsvTwoDimension())
+	#print ((trainLabel),(testLabel))
+	print((makeTrainingSet()))
 
