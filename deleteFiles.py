c4c8306f156f8cc19b919d018b7cfdf1b72b116a
dongwoLee <dongwoo4553@gmail.com>
2017-10-12 18:16:05 +0900

    modify

--- deleteFiles.py
+++ deleteFiles.py
@@ -1,28 +1,35 @@
-import pandas as pd
-import os, glob
-import shutil
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
-def getFileName(dir):
-	os.chdir(dir)
-	dateList = []
-	for file in glob.glob("*.csv"):
-		dateList.append(file[0:10])
+mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
+print(mnist.train.num_examples)
 
-	return dateList
 
-def moveCsv(file,DList):#Dlist is WholeDataList
-	locationData = pd.read_csv(file)
-	locationData = locationData['Date']
-
-	dst = "C:/Users/dw/Desktop/mosquito_cnn/Location_allDate/Yoonjung_Elementary"
-
-	temp = set(locationData) & set(DList)
-	temp = (sorted(temp,key=lambda d:tuple(map(int,d.split('-')))))
-
-	for i in range(len(temp)):
-		shutil.copy("C:/Users/dw/Desktop/mosquito_cnn/split_csv/noHeaderIndex/"+temp[i]+".csv",dst)
-
-if __name__ == '__main__':
-	wholeDataList = getFileName("C:/Users/dw/Desktop/mosquito_cnn/split_csv/noHeaderIndex/")
-	moveCsv("C:/Users/dw/Desktop/mosquito_cnn/Label_Data/Count_mosquito/Yoonjung_Elementary.csv",wholeDataList)
+# import pandas as pd
+# import os, glob
+# import shutil
+#
+# def getFileName(dir):
+# 	os.chdir(dir)
+# 	dateList = []
+# 	for file in glob.glob("*.csv"):
+# 		dateList.append(file[0:10])
+#
+# 	return dateList
+#
+# def moveCsv(file,DList):#Dlist is WholeDataList
+# 	locationData = pd.read_csv(file)
+# 	locationData = locationData['Date']
+#
+# 	dst = "C:/Users/dw/Desktop/mosquito_cnn/Location_allDate/Yoonjung_Elementary"
+#
+# 	temp = set(locationData) & set(DList)
+# 	temp = (sorted(temp,key=lambda d:tuple(map(int,d.split('-')))))
+#
+# 	for i in range(len(temp)):
+# 		shutil.copy("C:/Users/dw/Desktop/mosquito_cnn/split_csv/noHeaderIndex/"+temp[i]+".csv",dst)
+#
+# if __name__ == '__main__':
+# 	wholeDataList = getFileName("C:/Users/dw/Desktop/mosquito_cnn/split_csv/noHeaderIndex/")
+# 	moveCsv("C:/Users/dw/Desktop/mosquito_cnn/Label_Data/Count_mosquito/Yoonjung_Elementary.csv",wholeDataList)
 
--- mosq_cnn.py
+++ mosq_cnn.py
@@ -30,39 +30,39 @@ def makeTrainingSet():#training and test mosq
 	trainFolder = []
 	trainCsv = []
 	wholeCsv = []
-	for folder in os.listdir("/Users/leedongwoo/Desktop/mosquito_cnn/Location_allDate"):
+	for folder in os.listdir("C:/Users/dw/Desktop/mosquito_cnn/Location_allDate"):
 		if folder == '.DS_Store':
 			continue
 		else:
 			location.append(folder)#all location
 
 	for i in range(len(location)):
-		trainFolder.append(getFileName("/Users/leedongwoo/Desktop/mosquito_cnn/Location_allDate/"+str(location[i])))
+		trainFolder.append(getFileName("C:/Users/dw/Desktop/mosquito_cnn/Location_allDate/"+str(location[i])))
 
 	for j in range(len(trainFolder)):
 		for k in range(len(trainFolder[j])):
-			trainFolder[j][k] = "/Users/leedongwoo/Desktop/mosquito_cnn/Location_allDate/"+str(location[j])+"/"+str(trainFolder[j][k])
+			trainFolder[j][k] = "C:/Users/dw/Desktop/mosquito_cnn/Location_allDate/"+str(location[j])+"/"+str(trainFolder[j][k])
 
 	for p in range(len(trainFolder)):
 		for q in range(len(trainFolder[p])):
 			with open(trainFolder[p][q],'r') as f:
 				reader = csv.reader(f,delimiter=',')
-				 # this reader convert convert list to string
-				trainCsv.append(list(reader))
+				for row in reader:
+					trainCsv.append(row)
 
-	for index in range(len(trainCsv)):
-		wholeCsv.append([[float(y) for y in x] for x in trainCsv[index]])
+	# for index in range(len(trainCsv)):
+	# 	wholeCsv.append([[float(y) for y in x] for x in trainCsv[index]])
+    #
+	# trainingCsv = trainCsv[:int(len(trainCsv)*0.8)]
+	# testCsv  = trainCsv[int(len(trainCsv)*0.8):len(trainCsv)]
 
-	trainingCsv = trainCsv[:int(len(trainCsv)*0.8)]
-	testCsv  = trainCsv[int(len(trainCsv)*0.8):len(trainCsv)]
-
-	return array(trainingCsv),array(testCsv)
+	return 
 
 def makeLabel_level():#training level and test level 
 
 	train = []
 	test = []
-	wholeLabelCsv = getFileName("/Users/leedongwoo/Desktop/mosquito_cnn/Label_Data/Level/noDateMosq")
+	wholeLabelCsv = getFileName("C:/Users/dw/Desktop/mosquito_cnn/Label_Data/Level/noDateMosq")
 	wholeLabel = readCsv(wholeLabelCsv)
 
 	train = wholeLabel[:int(len(wholeLabel)*0.8)]
@@ -84,16 +84,60 @@ def makeLabel_level():#training level and test level
 
 if __name__ == '__main__':
 	
-	trainingCsv , testCsv = makeTrainingSet() 
-	trainLabel,testLabel = makeLabel_level()#Do i have to change Label data using one-hot encoding?
-	print (type(trainLabel))
-	X = tf.placeholder(tf.float32, shape=[None,5400])
-	Y = tf.placeholder(tf.float32, shape=[None,9])
+	# trainingCsv , testCsv = makeTrainingSet() #len(trainingCsv)=8605
+	# trainLabel,testLabel = makeLabel_level()#Do i have to change Label data using one-hot encoding?
 
+	print(makeTrainingSet())
 
+# 	X = tf.placeholder(tf.float32, shape=[None,180,30,1])
+# 	Y = tf.placeholder(tf.float32, shape=[None,9])
+# 	keep_prob = tf.placeholder(tf.float32)
+#
+# 	W1 = tf.Variable(tf.random_normal([3,3,1,32],stddev=0.01))
+# 	L1 = tf.nn.conv2d(X,W1,strides=[1,1,1,1], padding='SAME')
+# 	L1 = tf.nn.relu(L1)
+# 	L1 = tf.nn.max_pool(L1,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+#
+# 	W2 = tf.Variable(tf.random_normal([3,3,32,64],stddev=0.01))
+# 	L2 = tf.nn.conv2d(L1,W2,strides=[1,1,1,1],padding='SAME')
+# 	L2 = tf.nn.relu(L2)
+# 	L2 = tf.nn.max_pool(L2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+#
+# 	W3 = tf.Variable(tf.random_normal([45*8*64,256],stddev=0.01))
+# 	L3 = tf.reshape(L2,[-1,45*8*64])
+# 	L3 = tf.matmul(L3,W3)
+# 	L3 = tf.nn.relu(L3)
+# 	L3 = tf.nn.dropout(L3,keep_prob)
+#
+# 	W4 = tf.Variable(tf.random_normal([256,9],stddev=0.01))
+# 	model = tf.matmul(L3,W4)
+#
+# 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=model, labels=Y))
+# 	optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+#
+# 	init = tf.global_variables_initializer()
+# 	sess = tf.Session()
+# 	sess.run(init)
+#
+# 	batch_size = 100
+# 	total_data_len = 10757
+# 	total_batch = int(total_data_len / batch_size)
+#
+# 	for epoch in range(15):
+# 		total_cost = 0
+#
+# 		for i in range(total_batch):
+# 			batch_xs = trainingCsv[i]
+# 			batch_ys = trainLabel[i]
+#
+# 			batch_xs = batch_xs.reshape(1,180,30,1)
+# 			_, cost_val = sess.run([optimizer,cost],feed_dict={X:batch_xs, Y: batch_ys, keep_prob:0.7})
+#
+# 			total_cost += cost_val
+#
+# 		print('Epoch:', '%04d' % (epoch + 1),
+# 			  'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
+#
+# print('최적화 완료!')
 
 	
-
-
-
-
